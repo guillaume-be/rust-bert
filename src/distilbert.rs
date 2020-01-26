547f4e3d2b77835d06e5f6995885d9dc2459540a
Guillaume B <guillaume.becquin@gmail.com>
2020-01-26 11:36:36 +0100

    Initial implementation of embeddings

--- src/distilbert.rs
+++ src/distilbert.rs
@@ -18,7 +18,8 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
 use serde::{Deserialize, Serialize};
-
+use self::tch::nn::{ModuleT, embedding};
+use tch::Kind;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Activation {
@@ -30,17 +31,17 @@ pub enum Activation {
 pub struct DistilBertConfig {
     activation: Activation,
     attention_dropout: f32,
-    dim: usize,
+    dim: i64,
     dropout: f32,
-    hidden_dim: usize,
+    hidden_dim: i64,
     id2label: HashMap<i32, String>,
     initializer_range: f32,
     is_decoder: bool,
     label2id: HashMap<String, i32>,
-    max_position_embeddings: usize,
-    n_heads: usize,
-    n_layers: usize,
-    num_labels: usize,
+    max_position_embeddings: i64,
+    n_heads: i64,
+    n_layers: i64,
+    num_labels: i64,
     output_attentions: bool,
     output_hidden_states: bool,
     output_past: bool,
@@ -50,7 +51,7 @@ pub struct DistilBertConfig {
     tie_weights: bool,
     torchscript: bool,
     use_bfloat16: bool,
-    vocab_size: usize,
+    vocab_size: i64,
 }
 
 impl DistilBertConfig {
@@ -62,13 +63,33 @@ impl DistilBertConfig {
     }
 }
 
-fn gelu(x: Tensor) -> Tensor {
+fn _gelu(x: Tensor) -> Tensor {
     &x * 0.5 * (1.0 + (x / ((2.0 as f64).sqrt())).erf())
 }
 
 //fn create_sinusoidal_embeddings() {}
 
-struct Embeddings {
-    word_embeddings: nn::Embedding,
-    position_embeddings: nn::Embedding
+
+pub fn embeddings(p: nn::Path, config: DistilBertConfig) -> impl ModuleT {
+    let word_embeddings: nn::Embedding = embedding(&p / "word_embeddings",
+                                                   config.vocab_size,
+                                                   config.dim,
+                                                   Default::default());
+    let position_embeddings: nn::Embedding = embedding(&p / "position_embeddings",
+                                                       config.max_position_embeddings,
+                                                       config.dim,
+                                                       Default::default());
+
+    nn::func_t(move |input_ids, _train| {
+        let seq_length = (&input_ids).size().last().unwrap().to_owned();
+        let position_ids = Tensor::arange(seq_length, (Kind::Int64, input_ids.device()));
+        let position_ids = position_ids.unsqueeze(0).expand_as(input_ids);
+
+        let word_embed = input_ids.apply(&word_embeddings);
+        let position_embed = position_ids.apply(&position_embeddings);
+
+        let embeddings = word_embed + position_embed;
+
+        embeddings
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
-use crate::distilbert::DistilBertConfig;
+use crate::distilbert::{DistilBertConfig, embeddings};
 use std::path::Path;
 use std::env;
-
+use std::sync::Arc;
+use rust_transformers::preprocessing::vocab::base_vocab::Vocab;
+use rust_transformers::bert_tokenizer::BertTokenizer;
+use rust_transformers::preprocessing::tokenizer::base_tokenizer::{Tokenizer, TruncationStrategy};
+use tch::{Device, nn, Tensor};
 mod distilbert;
-
 
 fn main() {
 
+//    Config & set-up var store
     let config_path = env::var("distilbert_config_path").unwrap();
     let config_path = Path::new(&config_path);
-
     let config = DistilBertConfig::from_file(config_path);
-    let cuda_available = tch::Cuda::cudnn_is_available();
-    println!("{:?}", cuda_available);
+//    println!("{:?}", config);
+    let device = Device::cuda_if_available();
+    let vs = nn::VarStore::new(device);
 
-    println!("{:?}", config);
+//    Creation of tokenizer
+    let vocab_path = "E:/Coding/backup-rust/rust-transformers/resources/vocab/bert-base-uncased-vocab.txt";
+    let vocab = Arc::new(rust_transformers::BertVocab::from_file(vocab_path));
+    let tokenizer: BertTokenizer = BertTokenizer::from_existing_vocab(vocab.clone());
+
+//    Creation of sample input for testing purposes
+    let input = "Hello, world! This is a tokenization test";
+    let tokenized_input = tokenizer.encode(input, None, 128, &TruncationStrategy::LongestFirst, 0);
+
+//    Pass tokenized input through embeddings
+    let embeddings = embeddings(vs.root(), config);
+    let input_tensor = Tensor::of_slice(&tokenized_input.token_ids).unsqueeze(0).to(device);
+
+    let output = input_tensor.apply_t(&embeddings, true);
+    println!("{:?}", output);
 
 }
