aa819284df53367e680113e9e67fa0c280129f05
Guillaume B <guillaume.becquin@gmail.com>
2020-02-25 18:39:08 +0100

    updated license headers

--- examples/distilbert_masked_lm.rs
+++ examples/distilbert_masked_lm.rs
@@ -1,3 +1,15 @@
+// Copyright 2019-present, the HuggingFace Inc. team, The Google AI Language Team and Facebook, Inc.
+// Copyright 2019 Guillaume Becquin
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::path::PathBuf;
 use tch::{Device, Tensor, nn, no_grad};
 use rust_bert::distilbert::distilbert::{DistilBertModelMaskedLM, DistilBertConfig};
