89b486fb1277610792dd39c0e978c192a7cb60b1
Guillaume B <guillaume.becquin@gmail.com>
2020-03-01 11:29:57 +0100

    Added integration test for OpenAI GPT

--- tests/openai_gpt.rs
+++ tests/openai_gpt.rs
@@ -1,27 +1,12 @@
-// Copyright 2019-present, the HuggingFace Inc. team, The Google AI Language Team and Facebook, Inc.
-// Copyright 2019 Guillaume Becquin
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-extern crate failure;
-extern crate dirs;
-
 use std::path::PathBuf;
 use tch::{Device, nn, Tensor};
 use rust_tokenizers::{TruncationStrategy, Tokenizer, OpenAiGptTokenizer};
+use rust_bert::gpt2::gpt2::Gpt2Config;
 use rust_bert::common::config::Config;
 use rust_bert::openai_gpt::openai_gpt::OpenAIGPTLMHeadModel;
-use rust_bert::Gpt2Config;
 
-
-fn main() -> failure::Fallible<()> {
+#[test]
+fn openai_gpt_lm_model() -> failure::Fallible<()> {
     //    Resources paths
     let mut home: PathBuf = dirs::home_dir().unwrap();
     home.push("rustbert");
@@ -66,8 +51,11 @@ fn main() -> failure::Fallible<()> {
 
     let next_word_id = output.get(0).get(-1).argmax(-1, true).int64_value(&[0]);
     let next_word = tokenizer.decode(vec!(next_word_id), true, true);
-    println!("Provided input: {}", input[0]);
-    println!("Next word: {}", next_word);
+
+    assert_eq!(output.size(), vec!(1, 6, 40478));
+    assert!((output.double_value(&[0, output.size()[1] - 1, next_word_id]) - (9.1056)).abs() < 1e-4);
+    assert_eq!(next_word_id, 580i64);
+    assert_eq!(next_word, String::from("be"));
 
     Ok(())
 }
