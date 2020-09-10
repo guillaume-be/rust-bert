4978b94382f29a7a6009b7c870d19b4a8a2c7949
Guillaume B <guillaume.becquin@gmail.com>
2020-09-10 19:02:48 +0200

    Implemented two-stream attention

--- src/xlnet/attention.rs
+++ src/xlnet/attention.rs
@@ -19,6 +19,14 @@ use tch::nn::Init;
 use tch::{nn, Kind, Tensor};
 
 #[derive(Debug)]
+/// # Cache for XLNet attention layers
+/// Stores the cached value of the attention
+pub struct LayerState {
+    /// Cached content
+    pub prev_content: Tensor,
+}
+
+#[derive(Debug)]
 pub struct XLNetRelativeAttention {
     num_attention_heads: i64,
     attention_head_size: i64,
@@ -35,6 +43,7 @@ pub struct XLNetRelativeAttention {
     r_w_bias: Tensor,
     seg_embed: Tensor,
     layer_norm: nn::LayerNorm,
+    scale: f64,
 }
 
 impl XLNetRelativeAttention {
@@ -115,6 +124,8 @@ impl XLNetRelativeAttention {
         };
         let layer_norm = nn::layer_norm(p / "layer_norm", vec![config.d_model], layer_norm_config);
 
+        let scale = 1f64 / ((config.d_head as f64).powf(0.5f64));
+
         XLNetRelativeAttention {
             num_attention_heads: config.n_head,
             attention_head_size: config.d_head,
@@ -131,6 +142,7 @@ impl XLNetRelativeAttention {
             r_w_bias,
             seg_embed,
             layer_norm,
+            scale,
         }
     }
 
@@ -158,6 +170,7 @@ impl XLNetRelativeAttention {
         k_head_r: &Tensor,
         seg_mat: Option<&Tensor>,
         attention_mask: Option<&Tensor>,
+        train: bool,
     ) -> (Tensor, Option<Tensor>) {
         let ac = Tensor::einsum("ibnd,jbnd->bnij", &[&(q_head + &self.r_w_bias), &k_head_h]);
         let bd = self.rel_shift_bnij(
@@ -174,6 +187,120 @@ impl XLNetRelativeAttention {
                 Tensor::einsum("ijbs,ibns->bnij", &[seg_mat, &ef])
             }
             None => Tensor::zeros(&[1], (Kind::Float, ac.device())),
+        };
+
+        let mut attention_score = (ac + bd + ef) * self.scale;
+        if let Some(value) = attention_mask {
+            attention_score = attention_score - value.permute(&[2, 3, 0, 1]) * 1e30;
+        };
+
+        let attention_probas = attention_score
+            .softmax(3, Kind::Float)
+            .apply_t(&self.dropout, train);
+
+        let attention_vector = Tensor::einsum("bnij,jbnd->ibnd", &[&attention_probas, v_head_h]);
+
+        if self.output_attentions {
+            (
+                attention_vector,
+                Some(attention_probas.permute(&[2, 3, 0, 1])),
+            )
+        } else {
+            (attention_vector, None)
+        }
+    }
+
+    fn post_attention(
+        &self,
+        h: &Tensor,
+        attention_vector: &Tensor,
+        residual: bool,
+        train: bool,
+    ) -> Tensor {
+        let mut attention_out = Tensor::einsum("ibnd,hnd->ibh", &[attention_vector, &self.o])
+            .apply_t(&self.dropout, train);
+        if residual {
+            attention_out = attention_out + h;
+        };
+        attention_out.apply(&self.layer_norm)
+    }
+
+    pub fn forward_t(
+        &self,
+        h: &Tensor,
+        g: Option<&Tensor>,
+        attn_mask_h: Option<&Tensor>,
+        attn_mask_g: Option<&Tensor>,
+        r: &Tensor,
+        seg_mat: Option<&Tensor>,
+        mut layer_state: Option<LayerState>,
+        target_mapping: Option<&Tensor>,
+        train: bool,
+    ) {
+        let (output_g, output_h, attention_probas_g, attention_probas_h) = if g.is_some() {
+            let cat_value = if let Some(mems) = &layer_state {
+                if mems.prev_content.size().len() > 1 {
+                    Some(Tensor::cat(&[&mems.prev_content, h], 0))
+                } else {
+                    None
+                }
+            } else {
+                None
+            };
+            let cat = match &cat_value {
+                Some(value) => value,
+                None => h,
+            };
+
+            let q_head_h = Tensor::einsum("ibh,hnd->ibnd", &[h, &self.q]);
+            let k_head_h = Tensor::einsum("ibh,hnd->ibnd", &[cat, &self.k]);
+            let v_head_h = Tensor::einsum("ibh,hnd->ibnd", &[cat, &self.v]);
+            let k_head_r = Tensor::einsum("ibh,hnd->ibnd", &[r, &self.r]);
+
+            let (attention_vec_h, attention_probas_h) = self.rel_attention_core(
+                &q_head_h,
+                &k_head_h,
+                &v_head_h,
+                &k_head_r,
+                seg_mat,
+                attn_mask_h,
+                train,
+            );
+
+            let output_h = self.post_attention(h, &attention_vec_h, true, train);
+            let q_head_g = Tensor::einsum("ibh,hnd->ibnd", &[g.unwrap(), &self.q]);
+
+            let (attention_vec_g, attention_probas_g) = match target_mapping {
+                Some(target_mapping) => {
+                    let q_head_g = Tensor::einsum("mbnd,mlb->lbnd", &[&q_head_g, target_mapping]);
+                    let (attention_vec_g, attention_probas_g) = self.rel_attention_core(
+                        &q_head_g,
+                        &k_head_h,
+                        &v_head_h,
+                        &k_head_r,
+                        seg_mat,
+                        attn_mask_g,
+                        train,
+                    );
+                    let attention_vec_g =
+                        Tensor::einsum("lbnd,mlb->mbnd", &[&attention_vec_g, target_mapping]);
+                    (attention_vec_g, attention_probas_g)
+                }
+                None => self.rel_attention_core(
+                    &q_head_g,
+                    &k_head_h,
+                    &v_head_h,
+                    &k_head_r,
+                    seg_mat,
+                    attn_mask_g,
+                    train,
+                ),
+            };
+
+            let output_g = self.post_attention(g.unwrap(), &attention_vec_g, true, train);
+            (output_g, output_h, attention_probas_g, attention_probas_h)
+        } else {
+            //     ToDo
         };
     }
 }
