29dae0848b595835e4de97d395b839909aa03192
guillaume-be <guillaume.becquin@gmail.com>
2021-05-12 16:47:14 +0200

    Clean-up python model conversion utilities (#148)
    * Cleaned-up dependency conversion (now relies on model hub repositories)
    
    * Updated dependencies
    
    * Added requests dependency

--- utils/download-dependencies_distilbert.py
+++ utils/download-dependencies_distilbert.py
@@ -1,47 +1,35 @@
-from transformers import DISTILBERT_PRETRAINED_CONFIG_ARCHIVE_MAP
-from transformers.models.distilbert.tokenization_distilbert import PRETRAINED_VOCAB_FILES_MAP
-from transformers.file_utils import get_from_cache, hf_bucket_url
 from pathlib import Path
-import shutil
 import os
 import numpy as np
 import torch
 import subprocess
+import requests
 
-config_path = DISTILBERT_PRETRAINED_CONFIG_ARCHIVE_MAP["distilbert-base-uncased"]
-vocab_path = PRETRAINED_VOCAB_FILES_MAP["vocab_file"]["distilbert-base-uncased"]
-weights_path = "distilbert-base-uncased"
+if __name__ == "__main__":
 
-target_path = Path.home() / 'rustbert' / 'distilbert'
+    target_path = Path.home() / 'rustbert' / 'distilbert'
+    os.makedirs(str(target_path), exist_ok=True)
 
-temp_config = get_from_cache(config_path)
-temp_vocab = get_from_cache(vocab_path)
-temp_weights = get_from_cache(hf_bucket_url(weights_path, filename="pytorch_model.bin"))
+    weights_url = "https://huggingface.co/sshleifer/tiny-distilbert-base-cased/resolve/main/pytorch_model.bin"
+    r = requests.get(weights_url, allow_redirects=True)
+    (target_path / 'pytorch_model.bin').open('wb').write(r.content)
 
-os.makedirs(str(target_path), exist_ok=True)
+    weights = torch.load(target_path / 'pytorch_model.bin', map_location='cpu')
+    nps = {}
+    for k, v in weights.items():
+        nps[k] = np.ascontiguousarray(v.cpu().numpy())
 
-config_path = str(target_path / 'config.json')
-vocab_path = str(target_path / 'vocab.txt')
-model_path = str(target_path / 'model.bin')
+    np.savez(target_path / 'model.npz', **nps)
 
-shutil.copy(temp_config, config_path)
-shutil.copy(temp_vocab, vocab_path)
-shutil.copy(temp_weights, model_path)
+    source = str(target_path / 'model.npz')
+    target = str(target_path / 'model.ot')
 
-weights = torch.load(temp_weights, map_location='cpu')
-nps = {}
-for k, v in weights.items():
-    nps[k] = np.ascontiguousarray(v.cpu().numpy())
+    toml_location = (Path(__file__).resolve() / '..' / '..' / 'Cargo.toml').resolve()
 
-np.savez(target_path / 'model.npz', **nps)
+    subprocess.call(
+        ['cargo', 'run', '--bin=convert-tensor', '--manifest-path=%s' % toml_location, '--', source, target])
 
-source = str(target_path / 'model.npz')
-target = str(target_path / 'model.ot')
+    os.remove(str(target_path / 'pytorch_model.bin'))
+    os.remove(str(target_path / 'model.npz'))
 
-toml_location = (Path(__file__).resolve() / '..' / '..' / 'Cargo.toml').resolve()
-
-subprocess.call(
-    ['cargo', 'run', '--bin=convert-tensor', '--manifest-path=%s' % toml_location, '--', source, target])
-
-os.remove(str(target_path / 'model.bin'))
-os.remove(str(target_path / 'model.npz'))
+    assert (target_path / 'model.ot').exists(), "Conversion of the model failed."
