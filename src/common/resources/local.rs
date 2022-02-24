e83c77d2ed6e5dd60fd8e827bcc66e9df47d6c70
Guillaume Becquin <guillaume.becquin@gmail.com>
2022-02-24 18:20:28 +0000

    moved ResourceProvider to resources mod

--- src/common/resources/local.rs
+++ src/common/resources/local.rs
@@ -1,26 +1,6 @@
 use crate::common::error::RustBertError;
+use crate::resources::ResourceProvider;
 use std::path::PathBuf;
-
-/// # Resource Trait that can provide the location of the model, configuration or vocabulary resources
-pub trait ResourceProvider {
-    /// Provides the local path for a resource.
-    ///
-    /// # Returns
-    ///
-    /// * `PathBuf` pointing to the resource file
-    ///
-    /// # Example
-    ///
-    /// ```no_run
-    /// use rust_bert::resources::{LocalResource, ResourceProvider};
-    /// use std::path::PathBuf;
-    /// let config_resource = LocalResource {
-    ///     local_path: PathBuf::from("path/to/config.json"),
-    /// };
-    /// let config_path = config_resource.get_local_path();
-    /// ```
-    fn get_local_path(&self) -> Result<PathBuf, RustBertError>;
-}
 
 /// # Local resource
 #[derive(PartialEq, Clone)]
--- src/common/resources/mod.rs
+++ src/common/resources/mod.rs
@@ -18,10 +18,33 @@
 //! pre-trained models in each model module.
 
 mod local;
+
+use crate::common::error::RustBertError;
+pub use local::LocalResource;
+use std::path::PathBuf;
+
+/// # Resource Trait that can provide the location of the model, configuration or vocabulary resources
+pub trait ResourceProvider {
+    /// Provides the local path for a resource.
+    ///
+    /// # Returns
+    ///
+    /// * `PathBuf` pointing to the resource file
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// use rust_bert::resources::{LocalResource, ResourceProvider};
+    /// use std::path::PathBuf;
+    /// let config_resource = LocalResource {
+    ///     local_path: PathBuf::from("path/to/config.json"),
+    /// };
+    /// let config_path = config_resource.get_local_path();
+    /// ```
+    fn get_local_path(&self) -> Result<PathBuf, RustBertError>;
+}
+
 #[cfg(feature = "remote")]
 mod remote;
-
-pub use local::{LocalResource, ResourceProvider};
-
 #[cfg(feature = "remote")]
 pub use remote::RemoteResource;
