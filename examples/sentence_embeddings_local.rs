1ad5bdcfa5373416afb34150ae8152d7b9edfd08
Guillaume Becquin <guillaume.becquin@gmail.com>
2022-07-02 16:53:29 +0100

    Added conversion information for Distil-based sentence embedding models

--- examples/sentence_embeddings_local.rs
+++ examples/sentence_embeddings_local.rs
@@ -9,6 +9,19 @@ use rust_bert::pipelines::sentence_embeddings::SentenceEmbeddingsBuilder;
 ///   ```sh
 ///   python ./utils/convert_model.py resources/all-MiniLM-L12-v2/pytorch_model.bin
 ///   ```
+///
+/// For models missing the prefix in their saved weights (e.g. Distil-based models), the
+/// conversion needs to be updated to include this prefix so that the weights can be found:
+///   ```sh
+///   python ./utils/convert_model.py resources/path/to/pytorch_model.bin --prefix distilbert.
+///   ```
+///
+/// For models including a dense projection layer (e.g. Distil-based models), these weights
+/// need to be converted as well:
+/// ```sh
+///   python ../utils/convert_model.py  resources/path/to/2_Dense/pytorch_model.bin --suffix
+/// ```
+///
 fn main() -> anyhow::Result<()> {
     // Set-up sentence embeddings model
     let model = SentenceEmbeddingsBuilder::local("resources/all-MiniLM-L12-v2")
