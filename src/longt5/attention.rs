716e0e6e56f2683cdc6a3b86fa8fbd693e4fba77
Guillaume Becquin <guillaume.becquin@gmail.com>
2022-12-31 11:10:32 +0000

    LongT5 WiP: utility functions (2)

--- src/longt5/attention.rs
+++ src/longt5/attention.rs
@@ -10,7 +10,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use tch::Tensor;
+use tch::{Device, IndexOp, Kind, Tensor};
 
 fn pad_to_multiple(x: Tensor, block_length: i64, dim: usize, pad_value: f64) -> Tensor {
     let mut x_size = x.size();
@@ -40,4 +40,55 @@ fn split_into_blocks(mut x: Tensor, block_length: i64, dim: usize) -> Tensor {
     } else {
         x.reshape(x_size.as_slice())
     }
+}
+
+fn concatenate_3_blocks(
+    x: &Tensor,
+    block_dim: usize,
+    sequence_dim: i64,
+    pad_value: Option<f64>,
+) -> Tensor {
+    let x_size = x.size();
+    let num_blocks = x_size[block_dim];
+    let mut pad = vec![0i64; 2 * x.dim()];
+    pad[block_dim] = 1;
+    pad[block_dim + 1] = 1;
+    pad.reverse();
+    let x = x.pad(pad.as_slice(), "constant", pad_value.unwrap_or(0f64));
+    let mut block_list: Vec<Tensor> = Vec::with_capacity(3);
+    for i in 0..3 {
+        block_list.push(x.narrow(block_dim as i64, i, num_blocks));
+    }
+    Tensor::cat(block_list.as_slice(), sequence_dim)
+}
+
+fn make_3blocks_relative_position_ids(block_length: i64, device: Device) -> Tensor {
+    let position_ids = Tensor::arange(3 * block_length, (Kind::Int, device));
+    let center_position_ids = position_ids.i(block_length..2 * block_length);
+    position_ids.unsqueeze(0) - center_position_ids.unsqueeze(1)
+}
+
+fn mask_local_attention_mask(local_attention_mask: &Tensor, block_length: i64) -> Tensor {
+    let relative_position_ids =
+        make_3blocks_relative_position_ids(block_length, local_attention_mask.device());
+    let locality_mask = relative_position_ids
+        .abs()
+        .lt(block_length)
+        .unsqueeze(0)
+        .unsqueeze(0);
+    local_attention_mask.logical_and(&locality_mask)
+}
+
+fn get_local_attention_mask(attention_mask: Tensor, block_length: i64, device: Device) -> Tensor {
+    let blocked_attention_mask = split_into_blocks(attention_mask, block_length, 1);
+    let three_blocked_attention_mask = concatenate_3_blocks(&blocked_attention_mask, 1, 2, None);
+
+    let blocked_attention_mask = blocked_attention_mask.unsqueeze(-1);
+    let three_blocked_attention_mask = three_blocked_attention_mask.unsqueeze(-2);
+
+    let local_attention_mask = mask_local_attention_mask(
+        &blocked_attention_mask.logical_and(&three_blocked_attention_mask),
+        block_length,
+    );
+    local_attention_mask.unsqueeze(1)
 }
