e0577208167a62b22f22a390a68315501ea3d735
Guillaume Becquin <guillaume.becquin@gmail.com>
2023-03-12 16:19:55 +0000

    WIP GPT2

--- examples/onnxruntime-test.rs
+++ examples/onnxruntime-test.rs
@@ -1,38 +1,86 @@
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::Arc;
 
-use ndarray::{array, concatenate, s, Array1, Axis};
+use ndarray::Array2;
 use ort::{
-    download::language::machine_comprehension::GPT2,
-    tensor::{DynOrtTensor, FromArray, InputTensor, OrtOwnedTensor},
-    Environment, ExecutionProvider, GraphOptimizationLevel, OrtResult, SessionBuilder,
+    tensor::{FromArray, InputTensor},
+    Environment, ExecutionProvider, GraphOptimizationLevel, OrtResult, Session, SessionBuilder,
 };
-use rand::Rng;
 
-const GEN_TOKENS: i32 = 45;
-const TOP_K: usize = 5;
+struct NameMapping<'a> {
+    decoder_input_names: HashMap<&'a str, usize>,
+    decoder_output_names: HashMap<&'a str, usize>,
+    decoder_key_value_input_names: HashMap<&'a str, usize>,
+    decoder_key_value_output_names: HashMap<&'a str, usize>,
+}
+
+fn get_input_output_mapping(session: &Session) -> NameMapping {
+    let decoder_input_names = session
+        .inputs
+        .iter()
+        .enumerate()
+        .map(|(pos, input)| (input.name.as_str(), pos))
+        .collect::<HashMap<&str, usize>>();
+
+    let decoder_output_names = session
+        .outputs
+        .iter()
+        .enumerate()
+        .map(|(pos, output)| (output.name.as_str(), pos))
+        .collect::<HashMap<&str, usize>>();
+
+    let decoder_key_value_input_names = decoder_input_names
+        .iter()
+        .filter(|(name, _)| name.contains(".key") | name.contains(".value"))
+        .map(|(name, pos)| (*name, *pos))
+        .collect::<HashMap<&str, usize>>();
+
+    let decoder_key_value_output_names = decoder_output_names
+        .iter()
+        .filter(|(name, _)| name.contains(".key") | name.contains(".value"))
+        .map(|(name, pos)| (*name, *pos))
+        .collect::<HashMap<&str, usize>>();
+
+    NameMapping {
+        decoder_input_names,
+        decoder_output_names,
+        decoder_key_value_input_names,
+        decoder_key_value_output_names,
+    }
+}
 
 fn main() -> OrtResult<()> {
     tracing_subscriber::fmt::init();
 
-    let mut rng = rand::thread_rng();
-
     let environment = Arc::new(
         Environment::builder()
             .with_name("GPT2")
-            .with_execution_providers([ExecutionProvider::cuda()])
+            .with_execution_providers([ExecutionProvider::cpu()])
             .build()?,
     );
 
-    let session = SessionBuilder::new(&environment)?
+    let decoder_session = SessionBuilder::new(&environment)?
         .with_optimization_level(GraphOptimizationLevel::Level1)?
         .with_intra_threads(1)?
         .with_model_from_file(PathBuf::from(
             "E:/Coding/distilgpt2-onnx/decoder_model.onnx",
         ))?;
 
-    // let tokens = &mut Array1::from_iter(tokens.iter().cloned());
-    //
+    // ToDo: align the input array using the mapping
+    let decoder_name_mapping = get_input_output_mapping(&decoder_session);
+
+    let input_ids = Array2::from_shape_vec((1, 3), vec![8888i64, 318i64, 257i64]).unwrap();
+    let attention_mask = Array2::from_shape_vec((1, 3), vec![1i64, 1i64, 1i64]).unwrap();
+
+    let outputs = decoder_session.run([
+        InputTensor::from_array(input_ids.into_dyn()),
+        InputTensor::from_array(attention_mask.into_dyn()),
+    ])?;
+
+    // ToDo: extract the features using the mapping
+    println!("{:?}", outputs);
+
     // for _ in 0..GEN_TOKENS {
     //     let n_tokens = &tokens.shape()[0];
     //     let array = tokens
